dabdf7523bff4c98a5cdf09162a19514fa265e4d
svganeshch <svganesh.chilakalapudi@gmail.com>
2025-07-11 12:23:17 +0530

    some changes

--- Packages/com.jbooth.microsplat.core/Scripts/Editor/MicroSplatUtilities.cs
+++ Packages/com.jbooth.microsplat.core/Scripts/Editor/MicroSplatUtilities.cs
@@ -41,9 +41,9 @@ namespace JBooth.MicroSplat
       public static PipelineType DetectPipeline ()
       {
 #if UNITY_2019_1_OR_NEWER
-         if (GraphicsSettings.renderPipelineAsset != null) {
+         if (GraphicsSettings.defaultRenderPipeline != null) {
                // SRP
-               var srpType = GraphicsSettings.renderPipelineAsset.GetType().ToString();
+               var srpType = GraphicsSettings.defaultRenderPipeline.GetType().ToString();
                if (srpType.Contains("HDRenderPipelineAsset")) {
                   return PipelineType.HDPipeline;
                }
